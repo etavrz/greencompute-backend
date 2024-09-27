d430785576f7613c42cd8b0d49491ab73ad58a7e
Elias Tavarez <etav@umich.edu>
2024-09-27 18:32:00 -0500

    feat: add s3 client and buckets endpoint to test functionality

--- greencompute_backend/main.py
+++ greencompute_backend/main.py
@@ -1,6 +1,20 @@
+import os
+
+import boto3
 from fastapi import FastAPI
+from loguru import logger
 
 app = FastAPI()
+
+try:
+    client = boto3.client(
+        "s3",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name="us-east-1",  # e.g., "us-east-1"
+    )
+except Exception as e:
+    logger.error(f"Could not connect to S3: {e}")
 
 
 @app.get("/")
@@ -11,3 +25,9 @@ async def root():
 @app.get("/health")
 async def health():
     return {"status": "healthy"}
+
+
+@app.get("/buckets")
+async def list_buckets():
+    response = client.list_buckets()
+    return response
