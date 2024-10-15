0f9381a901d1cdb258632eef1f38a3769305fac5
Elias Tavarez <111210402+etavrz@users.noreply.github.com>
2024-10-15 18:24:45 -0500

    feat: add emissions data endpoint (#7)
    * feat: implement carbon-emissions model
    
    * feat: add emission data endpoint
    
    ---------
    
    Co-authored-by: Elias Tavarez <etav@umich.edu>

--- greencompute_backend/config.py
+++ greencompute_backend/config.py
@@ -8,3 +8,4 @@ ENVIRON = os.getenv("ENVIRON", "dev")
 ROOT_PATH = "/api" if ENVIRON == "prod" else ""
 AWS_S3_BUCKET = os.getenv("AWS_S3_BUCKET")
 CARBON_MODEL = "xgb_carbon_model.pkl"
+DATA_FILE = "cloud_embodied_emissions.csv"
